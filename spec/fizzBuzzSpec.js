0fb47fb30b463c50d53126a18ac82a37147ab9df
Lima Vadakara <lima.vadakara@gmail.com>
2019-03-04 16:56:03 +0000

    tidy up tests

--- spec/fizzBuzzSpec.js
+++ spec/fizzBuzzSpec.js
@@ -1,10 +1,10 @@
 describe("fizzBuzz", function() {
+
   it('prints fizz when given 3', function() {
     expect(fizzBuzz(3)).toEqual("Fizz");
-    });
-});
 
-describe("fizzBuzz", function() {
+  });
+
   it('prints buzz when given 5', function() {
     expect(fizzBuzz(5)).toEqual("Buzz");
 
